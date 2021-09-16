fac79f644be545fbb16cf7604fcf7dc1fdb44953
astephens-fsd <astephens.pm@icloud.com>
2021-09-16 13:18:07 -0700

    Completed Task 5

--- index.js
+++ index.js
@@ -165,8 +165,22 @@ class Instructor extends Lambdasian {
 //         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
 //         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 // */
-class Student {
-   
+class Student extends Lambdasian {
+   constructor({name, age, location, previousBackground, className, favSubjects}){
+     super({name, age, location, previousBackground, className, favSubjects});
+     this.previousBackground = previousBackground;
+     this.className = className;
+     this.favSubjects = favSubjects;
+   }
+   listSubjects(){
+    return `${this.favSubjects}`;
+   }
+   PRAssignment(subject){
+    return `${this.name} has submitted a PR for ${subject}`;
+   }
+   sprintChallenge(subject){
+    return `${this.name} has begun the sprint challenge on ${subject}`;
+   }
 }
 
 /*
